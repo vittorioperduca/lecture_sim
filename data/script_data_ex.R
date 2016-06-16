eba50965ec57c3fa0d1bac5f8b6209a3ca661703
vittorioperduca <vittorio.perduca@gmail.com>
2016-06-16 23:53:03 +0200

    first exercise done

--- data/script_data_ex.R
+++ data/script_data_ex.R
@@ -75,16 +75,66 @@ save(geno_d,pheno,file='data/ex_snps_d.Rda')
 
 
 #################
-# Exercice, 1000 genomes data
+# Exercise, 1000 genomes data
 #################
+#rm(list = ls())
 require(waffect)
 
 system('plink --file data/chrX_629 --recodeA --out data/geno')
 geno=read.table('data/geno.raw',header=TRUE)
-geno=geno[,7:1007]
+geno=geno[,7:1006]
 
-f0_s=1.2; OR_s=1.5
-pi_s=1/(1+exp(-log(f0)-log(OR)*geno[,150]-log(OR)*geno[,750]))
-pheno=waffect(prob=pi,label=c(1,0),count=315)
+#f0_s=1.2; OR1_s=1.5; OR2_s=1.7 #2, 1.5
+f0_s=0.4; OR1_s=1.6
+#pi_s=1/(1+exp(-log(f0_s)-log(OR1_s)*geno[,150]-log(OR2_s)*geno[,950]))
+pi_s=1/(1+exp(-log(f0_s)-log(OR1_s)*geno[,150]))
+pheno=waffect(prob=pi_s,label=c(1,0),count=314)
 
-save(geno,pheno, pi_s, f0_s, OR_s, file='data/exercise_geno.Rda')
+#save(geno,pheno, pi_s, f0_s, OR1_s, OR2_s,file='data/exercise_geno6.Rda')
+save(geno,pheno, pi_s, f0_s, OR1_s,file='data/exercise_geno.Rda')
+
+###################################################################################
+
+int=c
+
+
+
+
+#DA QUI IN POI PULIRE
+##################
+# Exercise, 1000 genomes data, environmental exposure
+##################
+
+#########
+#Power study case-contol
+#########
+load('data/exercise_geno.Rda')
+cores <- detectCores()
+N=500; p=ncol(geno); n=nrow(geno)
+
+e=sample(c(0,1),prob = c(0.5,0.5),size=n,replace=TRUE)
+maf=0.2; geno[,150]=sample(0:1,size=n,replace=TRUE,prob=c((1-maf)^2,2*maf*(1-maf)+(maf)^2))
+pi_e=1/(1+exp(-log(0.05/(1-0.05))-log(1.5)*geno[,150]-log(2)*e-log(1.5)*geno[,150]*e))
+
+require(waffect)
+one.sim_caco=function(i){
+  perm=waffect(pi_e,label=c(1,0),count=315)
+  out=summary(glm(perm~geno[,150]*e,family=binomial(link='logit')))$coef[4,4]
+  return(out)
+}
+pvalue_150_caco=mclapply(1:N, one.sim_caco, mc.cores=cores)
+pvalue_150_caco=as.numeric(pvalue_150_caco)
+mean(pvalue_150_caco<=0.05)
+
+#########
+#Power study case only
+#########
+one.sim_ca=function(i){
+  perm=waffect(pi_e,label=c(1,0),count=315)
+  new.data=geno[which(perm==1),]
+  out=summary(glm(e~geno[,150],family=binomial(link='logit')))$coef[2,4]
+  return(out)
+}
+pvalue_150_ca=mclapply(1:N, one.sim_ca, mc.cores=cores)
+pvalue_150_ca=as.numeric(pvalue_150_ca)
+mean(pvalue_150_ca<=0.05)
